5ab28c4399479289bc0aa242b489e218e5c8a525
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-25 18:14:34 +0900

    アソシエーションの定義

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2020_11_25_075146) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -33,6 +32,5 @@ ActiveRecord::Schema.define(version: 2020_11_25_075146) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
 end
