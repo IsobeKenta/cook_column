371c908ca79777973b274d3f39ce358ff4cb8ff6
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-27 19:51:11 +0900

    動画投稿機能実装

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,10 +1,33 @@
 class Cook < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :image
+  has_one_attached :video
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
 
-  validates :title, :text, presence: true
+  validates :title, presence: true
+  validates :text ,presence: true, unless: :was_attached?
   validates :genre_id, numericality: { other_than: 1 }
+
+  def was_attached?
+    self.image.attached?
+    self.video.attached?
+  end
+  validate :image_presence
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    end
+  end
+  validate :video_presence
+  def video_presence
+    if video.attached?
+      if !video.content_type.in?(%('video/mp4 video/mov'))
+        errors.add(:video, 'にはmp4またはmovファイルを添付してください')
+      end
+    end
+  end
 end
